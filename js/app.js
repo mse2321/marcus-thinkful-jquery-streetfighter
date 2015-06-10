f6dfeb1540cc54c269d08d7d7b9b5e4262e4b8f8
mse2321 <mse233@gmail.com>
2015-06-09 21:42:47 -0400

    updates to js file

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+ 	$('.ryu').mouseenter(function() {
+	    $('.ryu-still').hide();
+	    $('.ryu-ready').show();
+  	})
+  	.mouseleave(function() {
+	    $('.ryu-ready').hide();
+	    $('.ryu-still').show();
+ 	 })
+  	.mousedown(function() {
+	    // play hadouken sound
+	    $('.ryu-ready').hide();
+	    $('.ryu-throwing').show();
+	    $('.hadouken').show().animate(
+		  {'left': '1020px'},
+		  500,
+		function() 
+			{
+			$(this).hide();
+			$(this).css('left', '520px');
+  			}
+		);
+  	})
+  	.mouseup(function() {
+  		$('.ryu-throwing').hide();
+  		$('.ryu-ready').show();
+	});
+});
